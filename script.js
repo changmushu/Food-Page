3955e3e743f7b008b22721399050f0849ca7ce47
changmushu <1173727758@qq.com>
2023-03-23 19:29:42 +0800

    提交

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuIcon = document.querySelector(".menu-icon")
+const lines = document.querySelector(".lines")
+const container = document.querySelector(".container")
+
+
+menuIcon.addEventListener("click",()=>{
+    container.classList.toggle("change");
+})
